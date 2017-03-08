d86deef1a2f1a3755063779520534ac2d601ce89
James Annal <jamesannal@gmail.com>
2017-03-08 19:22:47 +0000

    add changes for app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory }from 'react-router'
+import { render } from 'react-dom'
+import Main from './components/Main.jsx';
+import Home from './components/Home.jsx';
+import Planets from './components/Planets.jsx';
+import People from './components/People.jsx';
+import Ships from './components/Ships.jsx';
 
 window.onload = () => {
   render(
-    <h1> App Started </h1>,
+    <Router history={hashHistory}>
+      <Route path='/' component ={Main}>
+        <IndexRoute component={Home}/>
+        <Route path='home' component={Home}/>
+        <Route path='planets' component={Planets}/>
+        <Route path='people' component={People}/>
+        <Route path='ships' component={Ships}/>
+      </Route>
+    </Router>,
     document.getElementById('app')
   )
 }
