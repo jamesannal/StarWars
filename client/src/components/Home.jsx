5e0638c78cd01c39fcdae1079fff85686ff224d1
James Annal <jamesannal@gmail.com>
2017-03-08 19:28:19 +0000

    add changes for home.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component{
+  render(){
+    return(
+      <div>
+        <h4>Star Wars Stuff</h4>
+        <p>This is a home page, in case you can't tell</p>
+      </div>
+    )
+  }
+}
+
+export default Home
